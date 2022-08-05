1916c985250af9285979a39f97f1e1fc6b565596
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-08-05 15:42:13 -0300

    feat: process method - req 03

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,24 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
+
+
+def process(path_file, instance: Queue):
+    for index in range(len(instance)):
+        data = instance.search(index)
+
+        if data["nome_do_arquivo"] == path_file:
+            return None
+
+    contents = txt_importer(path_file)
+
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(contents),
+        "linhas_do_arquivo": contents,
+    }
+
+    instance.enqueue(data)
+    print(data)
 
 
 def remove(instance):
