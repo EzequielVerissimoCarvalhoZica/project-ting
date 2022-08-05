fb16a04546a09aef0701534280ffeff7d1877eac
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-08-05 16:41:19 -0300

    feat: existis_word method - req 06

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,28 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+
+
+def exists_word(word: str, instance: Queue):
+    data = list()
+
+    for index in range(len(instance)):
+        content = instance.search(index)
+        find_lines = list()
+
+        for index, line in enumerate(content["linhas_do_arquivo"]):
+            line_lower = line.lower()
+            if line_lower.find(word.lower()) == -1:
+                continue
+            else:
+                find_lines.append({"linha": index + 1})
+
+        if len(find_lines) > 0:
+            data.append({
+                "palavra": word,
+                "arquivo": content["nome_do_arquivo"],
+                "ocorrencias": find_lines
+            })
+
+    return data
 
 
 def search_by_word(word, instance):
