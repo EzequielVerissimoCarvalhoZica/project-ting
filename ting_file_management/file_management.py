bfb054e08131102e6c2590161be51659fd42f336
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-08-05 15:19:22 -0300

    feat: txt_importer method - req 02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+from pydoc import resolve
+import sys
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file.endswith(".txt"):
+        try:
+            with open(path_file) as f:
+                content: str = f.read()
+                return content.split("\n")
+        except FileNotFoundError:
+            print(f"Arquivo {path_file} não encontrado", file = sys.stderr)     
+    else:
+        print("Formato inválido", file = sys.stderr)
